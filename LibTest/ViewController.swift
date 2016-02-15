a46769b54961143baf407d42556155c726875b38
河野 かおり <kono@mediba.jp>
2016-02-15 15:59:53 +0900

    Initial Commit

---
+++ LibTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LibTest
+//
+//  Created by 河野 かおり on 2016/02/15.
+//  Copyright © 2016年 K.K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
